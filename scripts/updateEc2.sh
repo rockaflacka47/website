32887082256cca2ef76009f7e5d7ef1aa3b4505d
david.rocker <david.rocker@mykaarma.com>
2023-02-10 11:56:49 +0100

    updated image and script

--- scripts/updateEc2.sh
+++ scripts/updateEc2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo rm -rf -v !(*.htaccess) /var/www/html/*
 
-sudo cp -r ~/mywebsite/mywebsite/dist/* /var/www/html
+sudo cp -r ~/website/dist/* /var/www/html
 
 sudo systemctl restart apache2.service
